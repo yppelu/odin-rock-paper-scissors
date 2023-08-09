a5f0a0d796c0dd01f0eca336b38e43aa5ab9221c
yppelu <yppelu@gmail.com>
2023-08-09 21:12:29 +0700

    Add a function that imitates a round

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/*
+** The object contains pairs choice: number
+** Numbers are required to access an array
+** The array holds values indicating at winner and loser
+*/
+const choices = {
+  'Rock': 0,
+  'Paper': 1,
+  'Scissors': 2
+}
+// 2D array with Rock-Paper-Scissors order; player in rows, computer in columns
+const loserWinner = [
+  [0, 1, -1],
+  [-1, 0, 1],
+  [1, -1, 0]
+]
+
 // Returns a string with a random computer choice
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
@@ -8,5 +25,22 @@ function getComputerChoice() {
       return ('Paper');
     case 2:
       return ('Scissors');
+  }
+}
+
+/*
+** Compares player's and computer's choices
+** and returns a string saying who won and how
+*/
+function playOneRound(playerChoice, computerChoice) {
+  let playerChoiceNum = choices[playerChoice];
+  let computerChoiceNum = choices[computerChoice];
+  switch (loserWinner[playerChoiceNum][computerChoiceNum]) {
+    case 1:
+      return (`You win! ${playerChoice} beats ${computerChoice}.`);
+    case 0:
+      return (`It is a tie! ${playerChoice} and ${computerChoice}.`);
+    case -1:
+      return (`You lose! ${computerChoice} beats ${playerChoice}.`);
   }
 }
