0a887fc105fa050afdb082e21612005e3bca9659
yppelu <yppelu@gmail.com>
2023-08-12 20:11:03 +0700

    Suddenly finish the project

--- script.js
+++ script.js
@@ -15,68 +15,107 @@ const loserWinner = [
   [-1, 1, 0]
 ]
 
-// Returns a string with a random computer choice
+const firstOpenWindow = document.querySelector('.first-open');
+const gameInProgressWindow = document.querySelector('.game-in-progress');
+const gameFinishedWindow = document.querySelector('.game-finished');
+
+const startGameButton = document.querySelectorAll('.btn-start-game');
+const playerRockOption = document.querySelector('.player .btn-rock');
+const playerPaperOption = document.querySelector('.player .btn-paper');
+const playerScissorsOption = document.querySelector('.player .btn-scissors');
+
+const computerRockOption = document.querySelector('.computer .btn-rock');
+const computerPaperOption = document.querySelector('.computer .btn-paper');
+const computerScissorsOption = document.querySelector('.computer .btn-scissors');
+
+const scoreBlock = document.querySelector('.scores');
+const roundResult = document.querySelector('.round-result');
+const gameResult = document.querySelector('.game-result');
+
+// Highlights the button which computer chose
+// And returns a string representing computer choice
 function getComputerChoice() {
+  computerRockOption.classList.remove('chosen');
+  computerPaperOption.classList.remove('chosen');
+  computerScissorsOption.classList.remove('chosen');
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
     case 0:
-      return ('Rock');
+      computerRockOption.classList.add('chosen');
+      return 'Rock';
     case 1:
-      return ('Paper');
+      computerPaperOption.classList.add('chosen');
+      return 'Paper';
     case 2:
-      return ('Scissors');
+      computerScissorsOption.classList.add('chosen');
+      return 'Scissors';
   }
 }
 
-// Returns a word with the first letter in upper and the rest in lower case
-function capitalize(word) {
-  return (word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase());
-}
-
-// Prompt a player to choose and checks if an input is valid
-function getPlayerChoice() {
-  let playerChoice = prompt('Your choice', '');
-  if (playerChoice === null) {
-    alert('Cancelled');
-    return (1);
-  }
-  playerChoice = capitalize(playerChoice);
-  if (choices[playerChoice] === undefined) {
-    alert('Wrong input, try again');
-    return (2);
-  }
-  return (playerChoice);
-}
-
-/*
-** Compares player's and computer's choices
-** and returns a string saying who won and how
-*/
+// Compares player's and computer's choices
+// and returns a string saying who won and how
 function playOneRound(playerChoice, computerChoice) {
   let playerChoiceNum = choices[playerChoice];
   let computerChoiceNum = choices[computerChoice];
   switch (loserWinner[playerChoiceNum][computerChoiceNum]) {
     case 1:
-      return (`You win! ${playerChoice} over ${computerChoice}.`);
+      roundResult.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
+      playerWins++;
+      scoreBlock.textContent = `Score: ${playerWins}:${computerWins}`;
+      finishGame(playerWins, computerWins);
+      break;
     case 0:
-      return (`It is a tie! ${playerChoice} and ${computerChoice}.`);
+      roundResult.textContent = `It is a tie! ${playerChoice} and ${computerChoice}.`;
+      playerWins++;
+      computerWins++;
+      scoreBlock.textContent = `Score: ${playerWins}:${computerWins}`;
+      finishGame(playerWins, computerWins);
+      break;
     case -1:
-      return (`You lose! ${computerChoice} over ${playerChoice}.`);
+      roundResult.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
+      computerWins++;
+      scoreBlock.textContent = `Score: ${playerWins}:${computerWins}`;
+      finishGame(playerWins, computerWins);
+      break;
   }
 }
 
-// Plays five rounds
-function game() {
-  let playerChoice, computerChoice;
-  for (let i = 0; i < 5; i++) {
-    playerChoice = getPlayerChoice();
-    if (playerChoice === 1) {
-      return;
-    } else if (playerChoice === 2) {
-      i--;
-    } else {
-      computerChoice = getComputerChoice();
-      console.log(playOneRound(playerChoice, computerChoice));
-    }
+function finishGame(playerWins, computerWins) {
+  if (playerWins === 5) {
+    gameInProgressWindow.style.display = 'none';
+    gameFinishedWindow.style.display = 'block';
+    gameResult.textContent = `It is a victory! ${playerWins}:${computerWins}! Congratulations!`;
+  }
+  if (computerWins === 5) {
+    gameInProgressWindow.style.display = 'none';
+    gameFinishedWindow.style.display = 'block';
+    gameResult.textContent = ` Defeat! ${playerWins}:${computerWins}! Such a pity!`;
   }
 }
+
+let computerWins;
+let playerWins;
+
+startGameButton.forEach((startButton) => startButton.addEventListener('click', () => {
+  computerWins = 0;
+  playerWins = 0;
+  computerRockOption.classList.remove('chosen');
+  computerPaperOption.classList.remove('chosen');
+  computerScissorsOption.classList.remove('chosen');
+  firstOpenWindow.style.display = 'none';
+  gameFinishedWindow.style.display = 'none';
+  gameInProgressWindow.style.display = 'block';
+}));
+
+playerRockOption.addEventListener('click', () => {
+  playOneRound('Rock', getComputerChoice());
+  roundCount++;
+});
+playerPaperOption.addEventListener('click', () => {
+  playOneRound('Paper', getComputerChoice());
+  roundCount++;
+});
+playerScissorsOption.addEventListener('click', () => {
+  playOneRound('Scissors', getComputerChoice());
+  roundCount++;
+});
